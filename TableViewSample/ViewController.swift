62664b53d928b0b31c4404e093bebeeab59ad215
Stefan Gugarel <stefan@cocoanetics.com>
2017-08-11 15:31:50 +0200

    Now I got for iOS10 and iOS11

--- TableViewSample/ViewController.swift
+++ TableViewSample/ViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	//let specialOffset: CGFloat = -20
+	
+	let topInset: CGFloat = 75 //- 20
+	
+	
+	// MARK: - Offsets for toolbar (iPhone)
+	
+	/// The content offset which hides the toolbar
+	fileprivate var offsetToolbarHidden: CGFloat {
+		return -tableView.contentInset.top + tableView.tableHeaderView!.bounds.height //- 20
+	}
+	
+	/// The content offset which shows the toolbar
+	fileprivate var offsetToolbarShown: CGFloat {
+		return -tableView.contentInset.top //- 20
+	}
+		
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		refreshControl?.backgroundColor = UIColor.red
+		tableView.refreshControl?.backgroundColor = UIColor.red
 		
-		refreshControl?.addTarget(self, action: #selector(self.refresh(_:)), for: UIControlEvents.valueChanged)
+		tableView.refreshControl?.addTarget(self, action: #selector(self.refresh(_:)), for: UIControlEvents.valueChanged)
 
-		self.tableView.contentInset = UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)
+		self.tableView.contentInset = UIEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
 		
 		self.navigationController?.navigationBar.isHidden = true
+		
+		tableView.refreshControl = UIRefreshControl()
+		tableView.refreshControl?.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		
+		super.viewWillAppear(animated)
+		
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -28,7 +57,28 @@ class ViewController: UITableViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-	public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+	
+	@objc func refresh(_ sender: UIRefreshControl) {
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+			
+			
+			self.tableView.refreshControl?.endRefreshing()
+			
+			let offset = CGPoint(x: 0, y: -self.topInset +  44)
+			
+			self.tableView.setContentOffset(offset, animated: true)
+			
+		}
+
+		
+	}
+}
+
+extension ViewController: UITableViewDataSource {
+	
+	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		
 		return 100
 		
@@ -36,9 +86,9 @@ class ViewController: UITableViewController {
 	
 	
 	
-	public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		
-		let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell") as! UITableViewCell
+		let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell")!
 		
 		cell.backgroundColor = UIColor.green
 		
@@ -46,20 +96,43 @@ class ViewController: UITableViewController {
 		
 	}
 	
-	@objc func refresh(_ sender: UIRefreshControl) {
-		
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-			
-			
-			self.refreshControl?.endRefreshing()
-			
-			let offset = CGPoint(x: 0, y: -100 + 44)
-			
-			self.tableView.setContentOffset(offset, animated: true)
-			
-		}
+}
 
+
+extension ViewController: UITableViewDelegate {
+	
+	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
 		
+		let offset = targetContentOffset.pointee.y
+		
+		guard scrollView.contentOffset.y < offsetToolbarHidden else {
+			
+			// If we are way down and scroll up with force we don't show toolbar
+			targetContentOffset.pointee.y = max(offset, offsetToolbarHidden)
+			return
+		}
+		
+		// If user lifts finger without velocity it scrolls to whatever is closer
+		if velocity.y == 0 {
+			
+			let topDistance = abs(offsetToolbarHidden - tableView.contentOffset.y)
+			let bottomDistance = abs(tableView.contentOffset.y - offsetToolbarShown)
+			
+			if topDistance > bottomDistance {
+				targetContentOffset.pointee.y = offsetToolbarShown
+			} else {
+				targetContentOffset.pointee.y = offsetToolbarHidden
+			}
+			
+			return
+		}
+		
+		// With velocity snap in the direction of the swipe
+		if velocity.y > 0  {
+			targetContentOffset.pointee.y = offsetToolbarHidden
+		} else {
+			targetContentOffset.pointee.y = offsetToolbarShown
+		}
 	}
 }
 
